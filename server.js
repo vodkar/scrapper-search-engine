4f57242f4369142d9212aa31a38b576e3dd1e453
vodkar <boombarah@mail.ru>
2021-07-01 20:17:08 +0500

    Поменял настройки запуска

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 const searchGoogle = require('./scrapper/searchGoogle');
 const searchInPage = require("./scrapper/searchInLinks");
