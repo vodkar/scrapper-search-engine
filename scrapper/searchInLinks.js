7cfec28128791f4f97ed64df577b736cd802b308
artemshalamov <iart1756@gmail.com>
2021-07-01 19:55:59 +0500

    little update

--- scrapper/searchInLinks.js
+++ scrapper/searchInLinks.js
@@ -6,9 +6,9 @@ const searchInPage = async (link) => {
         const browser = await puppeteer.launch({headless: true});
         let newPage = await browser.newPage();
         await newPage.goto(link);
-        dataObj.headers = await newPage.$eval('*', el => el.innerText);
-        // dataObj.headers = await newPage.$eval( 'h1', text => text.textContent);
-        // dataObj.text = await newPage.$eval('p', text => text.textContent);
+        dataObj.text = await newPage.$eval('*', el => el.innerText);
+        dataObj.url = link
+        dataObj.header = await newPage.$eval('h1', text => text.textContent);
         console.log('выполнил')
         await newPage.close();
     }
