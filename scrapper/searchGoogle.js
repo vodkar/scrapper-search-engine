2cb986b913878e211ec80c61b8663f9565a7270e
vodkar <boombarah@mail.ru>
2021-07-06 11:00:27 -0700

    Maybe its help

--- scrapper/searchGoogle.js
+++ scrapper/searchGoogle.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 const searchGoogle = async (searchQuery) => {
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto('https://google.com');
 
